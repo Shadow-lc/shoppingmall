f571ba16f3878099fd1e983a3e2c24d31c9249d3
键帽君 <1532185098@qq.com>
2019-11-08 21:08:51 +0800

    我也不知道写了个啥

--- src/main/java/com/yisen/shoppingmall/showgoods/goods/mapper/GoodsMapper.java
+++ src/main/java/com/yisen/shoppingmall/showgoods/goods/mapper/GoodsMapper.java
@@ -2,6 +2,7 @@ package com.yisen.shoppingmall.showgoods.goods.mapper;
 
 import com.yisen.shoppingmall.showgoods.goods.entity.Category;
 import com.yisen.shoppingmall.showgoods.goods.entity.Goods;
+import com.yisen.shoppingmall.showgoods.goods.entity.GoodsPic;
 import com.yisen.shoppingmall.showgoods.goods.vo.GoodsVo;
 import org.apache.ibatis.annotations.Param;
 
@@ -15,6 +16,14 @@ public interface GoodsMapper {
     List<Goods> querySome(GoodsVo vo);
 
     int delGoods(@Param("id") int id);
+
+    int addGoodsPic(GoodsPic pic);
+    int delGoodsPic(GoodsPic pic);
+    int updateGoodsPic(GoodsPic pic);
+    List<GoodsPic> querySome(@Param("goodsid") String goodsid);
+
+
+
 
     int addCa(Category ca);
 
